ec9cc11073ca4df89efe2299aaca6f8d263f4a26
Anwar Alqam <48700188+AnwarAlqam@users.noreply.github.com>
2026-04-09 06:21:31 -0500

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # a = 1
+        # b = 2
+        # c = 3
+        # ...
+        # act
+        # 1 3 7
+
+        anagrams = {} # key: summation, value: array of words
+        for word in strs:
+            key = ""
+            arr = ["0"] * 26
+
+            for char in word:
+                arr_index = ord(char) - ord('a')
+                arr[arr_index] = str(int(arr[arr_index]) + 1)
+            key = "".join(arr)
+
+            if key not in anagrams:
+                anagrams[key] = []
+            anagrams[key].append(word)
+
+        print(anagrams)
+        return list(anagrams.values())
