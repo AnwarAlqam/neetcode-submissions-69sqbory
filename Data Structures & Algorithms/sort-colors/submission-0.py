b919f59d4726c388b8550588ff35d8b6634f9d2e
Anwar Alqam <48700188+AnwarAlqam@users.noreply.github.com>
2026-04-24 21:40:57 -0500

    Add: sort-colors - submission-0

---
+++ Data Structures & Algorithms/sort-colors/submission-0.py
@@ -0,0 +1,55 @@
+class Solution:
+    def merge(self, arr, s: int, m: int, e: int):
+        #[1, 3]    [2, 4]
+        l_arr = arr[s:m+1]
+        r_arr = arr[m+1:e+1]
+        arr_index = s
+
+        l,r = 0,0
+
+        while l <= len(l_arr) - 1  and r <= len(r_arr) - 1:
+            if r_arr[r] < l_arr[l]:
+                arr[arr_index] = r_arr[r]
+                arr_index += 1
+                r += 1
+            elif l_arr[l] < r_arr[r]:
+                arr[arr_index] = l_arr[l]
+                arr_index += 1
+                l += 1
+            else:
+                arr[arr_index] = l_arr[l]
+                arr_index += 1
+                arr[arr_index] = r_arr[r]
+                arr_index += 1
+                r += 1
+                l += 1
+
+        if l <= len(l_arr) - 1:
+            while l <= len(l_arr) - 1:
+                arr[arr_index] = l_arr[l]
+                arr_index += 1
+                l += 1
+
+        if r <= len(r_arr) - 1:
+            while r <= len(r_arr) - 1:
+                arr[arr_index] = r_arr[r]
+                arr_index += 1
+                r += 1
+
+    def mergeSort(self, arr: List[int], s: int, e: int):
+        if e - s + 1 <= 1:
+            return arr
+
+        m = (s + e) // 2
+        self.mergeSort(arr, s, m)
+        self.mergeSort(arr, m + 1, e)
+        self.merge(arr, s, m, e)
+
+        return arr
+
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        self.mergeSort(nums, 0, len(nums) - 1)
+        
